975651669e87c71f6c66b70115d3fb01989e567c
yasse <yassenyasooo50@gmail.com>
2026-02-20 02:05:43 +0200

    first commit

--- site/src/jsMain/kotlin/org/example/garfend/models/Language.kt
+++
@@ -1,6 +0,0 @@
-package org.example.garfend.models
-
-enum class Language(val code: String, val displayName: String, val isRTL: Boolean) {
-    ENGLISH("en", "English", false),
-    ARABIC("ar", "العربية", true)
-}
